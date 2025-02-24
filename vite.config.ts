54e887d5c1cfe39c333948c208e52262f45764cf
Muhammad-Abdullah012 <not.muhammad.abdullah68@gmail.com>
2025-02-24 13:25:01 +0500

    fix: build issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+console.log("base url VITE_BASE => ", process.env.VITE_BASE)
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "/automation-template/"
+  base: process.env.VITE_BASE || '/',
 })
